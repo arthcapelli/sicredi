4a15f580ffb4527b6a46c94e6fa84ddb10f1814e
Arthur Alexandre Capelli Dos Santos <arthur.santos@cwi.com.br>
2023-04-06 19:25:28 -0300

    Ajustando estrutura inicial

---
+++ src/test/java/br/com/teste/sicredi/SicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
